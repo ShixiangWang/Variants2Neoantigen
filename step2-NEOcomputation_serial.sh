19682f1ac993993044f2780b71df0f7f3d37c4ff
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2018-06-21 19:31:52 +0800

    Update step2-NEOcomputation_serial.sh

--- step2-NEOcomputation_serial.sh
+++ step2-NEOcomputation_serial.sh
@@ -46,7 +46,8 @@ do
         $(grep $sampleID $PATH_HLA | awk '{print $2}') \
         $method $res_dir \
         -e $epitope_len \
-        -t -a sample_name \
+        -a sample_name \
+        -d 500 \
         --iedb-install-directory $PATH_MHC 
        
     source deactivate $py_env    
